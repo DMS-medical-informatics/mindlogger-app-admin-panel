1354ab979798cf23a3d550d3b95f4164ffa42bed
Erik Ilyin <erik.ilyin@aol.com>
2017-12-11 17:19:05 +0300

    activity chart fix

--- src/components/chart/ActivityChart.js
+++ src/components/chart/ActivityChart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import eventDrops from 'event-drops'
+import eventDrops from './eventDrops/'
 import * as d3 from 'd3'
+import {select, event as currentEvent} from 'd3-selection';
 const colors = d3.schemeCategory10
 
 export default class ActivityChart extends Component {
@@ -11,15 +12,21 @@ export default class ActivityChart extends Component {
 
     }
     renderChart = (node, data) => {
+        if(this.node) return
+        this.node = node
         const {start, result} = data
-        const {onSelect} = this.props
-        let chart = eventDrops()
-            .date(d => d.date)
-            .start(start)
-            .margin({ top: 60, left: 50, bottom: 40, right: 100 })
-            .labelsWidth(100)
-            .eventLineColor((d, i) => colors[i])
-            .click(onSelect)
+        const {onSelect, onHover, onOut} = this.props
+
+        let chart = eventDrops(
+            {date:(d => d.date),
+            start:(start),
+            margin:({ top: 60, left: 50, bottom: 40, right: 100 }),
+            labelsWidth:(100),
+            eventLineColor:((d, i) => colors[i]),
+            click:(onSelect),
+            mouseover:(data => { 
+                onHover(data, currentEvent)}),
+            })
         //var node = ReactDOM.findDOMNode(this)
         return d3.select(node).datum(result).call(chart)
     }
--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {BarChart} from 'recharts'
-import {Row, Col, Panel} from 'react-bootstrap'
+import {Row, Col, Panel, Popover, Modal, Button} from 'react-bootstrap'
+import moment from 'moment'
 
 import {base, storageRef} from '../../config/constants'
 import ActivityChart from '../chart/ActivityChart'
@@ -14,7 +15,8 @@ const getActivityChartData = (users, answers) => {
     let date = new Date(answer.updated_at)
     if(start > date)
       start = date
-    dict[key].push({...answer, date: date})
+    if(answer.updated_at)
+      dict[key].push({...answer, date: date})
   })
   let result = []
   Object.keys(dict).forEach(key => {
@@ -31,26 +33,97 @@ export default class Dashboard extends Component {
       state: 'surveys',
       defaultValue: [],
       asArray: true
-    });
-    base.bindToState("users", {
+    })
+    base.listenTo("users", {
       context: this,
-      state: 'users',
       defaultValue: [],
-    });
-    base.bindToState("answers", {
+      then: users => {
+        this.setState({users})
+        this.onData()
+      }}
+    );
+    base.listenTo("answers", {
       context: this,
-      state: 'answers',
       defaultValue: [],
-      asArray: true
+      asArray: true,
+      then: answers => {
+        this.setState({answers})
+        this.onData()
+      }
     });
   }
+
+  onData = () => {
+    const {answers, users} = this.state
+    let data = users && answers && getActivityChartData(users,answers)
+    this.setState({data})
+  }
+  
+
+  renderImage() {
+    const {answer} = this.state
+    return (
+        <Modal show={this.state.showModal} onHide={this.close}>
+            <Modal.Header closeButton>
+                <Modal.Title>Image Answer : {moment(answer.updated_at).format('llll')}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <div className="drawboard-container">
+                <img src={answer.image_url} className="drawboard-image" />
+                <div className="drawboard">
+                    <svg width="300" height="200">
+                    {answer.lines.map(this.renderLine)}
+                    </svg>
+                </div>
+            </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={this.close}>Close</Button>
+            </Modal.Footer>
+    </Modal>)
+
+}
+
+  onSelect = (data) => {
+    
+  }
+
+  onHover = (answer, event) => {
+    if(this.state.hover) return
+    if(this.outTimerId)
+      clearTimeout(this.outTimerId)
+    this.outTimerId = setTimeout(() => this.setState({hover: false}),2000)
+    this.cx = event.clientX
+    this.cy = event.clientY
+    this.setState({hover: true, answer})
+  }
+
+  onOut = (data) => {
+    this.outTimerId = setTimeout(() => this.setState({hover: false}),2000)
+  }
+
+  renderHover(){
+    const {answer, users} = this.state
+    console.log(answer)
+    let str = `${users[answer.participant].name} did ${answer.activity_type} activity. Check by clicking it`
+    console.log(str.length)
+    return(<Popover
+      id="popover-basic"
+      placement="bottom"
+      positionLeft={this.cx-140}
+      positionTop={this.cy}
+      title={moment(answer.updated_at).format('llll')}
+    > {users[answer.participant].name} did <strong>{answer.activity_type}</strong> activity. Check by clicking it
+    </Popover>
+    )
+  }
   render () {
-    const {surveys, answers, users} = this.state
+    const {surveys, answers, users, hover, data} = this.state
     // let data = [
     //   { name: "http requests", data: [{date: new Date('2014/09/15 13:24:54'), foo: 'bar1'}, {date: new Date('2014/09/15 13:25:03'), foo: 'bar2'}, {date: new Date('2014/09/15 13:25:05'), foo: 'bar1'}] },
     //   { name: "SQL queries", data: [{date: new Date('2014/09/15 13:24:57'), foo: 'bar4'}, {date: new Date('2014/09/15 13:25:04'), foo: 'bar6'}, {date: new Date('2014/09/15 13:25:04'), foo: 'bar2'}] }
     // ]
-    let data = users && answers && getActivityChartData(users,answers)
+    
     return (
       <div>
         <h2 className="text-center">Dashboard</h2>
@@ -75,10 +148,14 @@ export default class Dashboard extends Component {
         <Row>
           <Col xs={12}>
             <Panel header="Activities">
-              {data && <ActivityChart data={data} onSelect={data => console.log(data)} />}
+              <div style={{position:'relative'}} onMouseOver={this.mouseOver}>
+              {this.state.data && <ActivityChart data={this.state.data} onSelect={this.onSelect} onHover={this.onHover} onOut={this.onOut} />}
+              
+              </div>
             </Panel>
           </Col>
         </Row>
+        { hover && this.renderHover()}
       </div>
   )
   }
