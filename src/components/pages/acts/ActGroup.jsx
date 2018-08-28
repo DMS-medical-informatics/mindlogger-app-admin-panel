0c13437f339eec1db846d875f49db2e89eeb42f8
Jon Clucas <jon.clucas@childmind.org>
2018-08-28 18:12:13 -0400

    :pencil: :lipstick: :sparkles: "🛈 Activity information" Screens
    Ref #17

--- src/components/pages/acts/ActGroup.jsx
+++ src/components/pages/acts/ActGroup.jsx
@@ -20,11 +20,29 @@ class ActGroup extends Component {
     const info = (vol.meta.information["@id"] ? <Button onClick={() => onAdd(group)}>{vol.meta.information["@id"]}</Button> : <Button onClick={() => onAdd(group)}>[+]</Button>);
     const consent = (vol.meta.consent["@id"] ? <Button onClick={() => onAdd(group)}>{vol.meta.consent["@id"]}</Button> : <Button onClick={() => onAdd(group)}>[+]</Button>);
     return (
-      <Grid item>
-        <h4><strong>{vol.name} information screens:</strong> {info}</h4>
-        <h4><strong>{vol.name} consent:</strong> {consent}</h4>
-        <h4><strong>{vol.name} {group.name}</strong> <Button onClick={() => onAdd(group)}>[+]</Button><TextField type="search" className="search-text"/>&#128269;</h4>
-        { acts.map((act, i) => <div key={i}><Button onClick={() => onEdit(act)}>{act.name}</Button></div>) }
+      <Grid item xs={12}>
+        <Grid item>
+          <h4><strong>{vol.name} information screens:</strong> {info}</h4>
+        </Grid>
+        <Grid item>
+          <h4><strong>{vol.name} consent:</strong> {consent}</h4>
+        </Grid>
+        <Grid container xs={12}>
+          <Grid item xs={6}>
+            <h4><strong>{vol.name} {group.name}</strong> <Button onClick={() => onAdd(group)}>[+]</Button><TextField type="search" className="search-text"/>&#128269;</h4>
+          </Grid>
+          <Grid item xs={6}>
+            <strong>&#128712; Activity information</strong>
+          </Grid>
+        </Grid>
+        { acts.map((act, i) => (
+          <div key={i}>
+            <Grid container>
+              <Grid item xs={6}><Button onClick={() => onEdit(act)}>{act.name}</Button></Grid>
+              <Grid item xs={6}><Button onClick={() => onAdd(group)}>[{(act.meta && act.meta.instructions && act.meta.instructions["@id"]) ? <Button onClick={() => onAdd(group)}>{act.meta.instructions["@id"]}</Button> : '+'}]</Button></Grid>
+            </Grid>
+          </div>
+        )) }
       </Grid>
     )
   }
