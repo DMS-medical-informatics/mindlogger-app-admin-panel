2837329e01471395751e474ecd7b43bb0047ea50
Jon Clucas <jon.clucas@childmind.org>
2018-11-06 10:58:33 -0500

    :construction: :bug:  no such file or directory, open '/opt/codedeploy-agent/package.json'

--- CodeDeploy/start.sh
+++ CodeDeploy/start.sh
@@ -2,6 +2,8 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | b
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+ls
+pwd
 nvm install node
 npm install
 npm update
