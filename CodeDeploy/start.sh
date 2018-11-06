1e071c194ba26583d08e7b507138bba809ce156e
Jon Clucas <jon.clucas@childmind.org>
2018-11-06 12:44:44 -0500

    :pencil: :green_heart: :costruction: sudo nvm

--- CodeDeploy/start.sh
+++ CodeDeploy/start.sh
@@ -1,10 +1,10 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-export NVM_DIR="/home/ec2-user/.nvm"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | sudo bash
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 ls > ls.txt
 pwd > pwd.txt
-nvm install node
+sudo nvm install node
 sudo npm install
 sudo npm update
 sudo npm install node-sass install.js
